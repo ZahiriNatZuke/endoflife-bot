19f47fe40976b8bafd8831977e77deb3bb84b62f
Yohan González Almaguer <yohan.gonzalez.almaguer@gmail.com>
2023-09-03 18:37:27 -0400

    feat: complete basics integrations

--- src/bot/bot.update.ts
+++ src/bot/bot.update.ts
@@ -1,7 +1,7 @@
-import { Ctx, Hears, Start, Update } from '@grammyjs/nestjs';
+import { Command, Ctx, Hears, Start, Update } from "@grammyjs/nestjs";
 import { UseFilters } from '@nestjs/common';
 import { Context } from 'grammy';
-import { BotService } from "./bot.service";
+import { BotService } from './bot.service';
 import { GrammyExceptionFilter } from './filters';
 
 @Update()
@@ -19,7 +19,16 @@ export class BotUpdate {
     );
   }
 
-  @Hears(/^\/product (?<product>.*)/)
+  @Command('all')
+  onAll(@Ctx() ctx: Context) {
+    this.botService.allProducts().subscribe(({ data }) => {
+      return ctx.reply(`\`${JSON.stringify(data, null, 2)}\``, {
+        parse_mode: 'MarkdownV2',
+      });
+    });
+  }
+
+  @Hears(/^\/product (?<product>\S+)$/)
   onProduct(@Ctx() ctx: Context) {
     const product = ctx.match[1];
     this.botService.allDetails(product).subscribe(({ data }) => {
@@ -27,5 +36,18 @@ export class BotUpdate {
         parse_mode: 'MarkdownV2',
       });
     });
+  }
+
+  @Hears(/^\/product (?<product>\S+) (?<cycle>\S+)$/)
+  onCycle(@Ctx() ctx: Context) {
+    const product = ctx.match[1];
+    const cycle = ctx.match[2];
+    this.botService
+      .cycleDetailOfProduct(product, cycle)
+      .subscribe(({ data }) => {
+        return ctx.reply(`\`${JSON.stringify(data, null, 2)}\``, {
+          parse_mode: 'MarkdownV2',
+        });
+      });
   }
 }
