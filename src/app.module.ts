48ecc2e6795b4b06886a0e241cc649995e902cd5
Yohan González Almaguer <yohan.gonzalez.almaguer@gmail.com>
2023-09-09 16:57:38 -0400

    feat: inclde session middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { NestjsGrammyModule } from '@grammyjs/nestjs';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { session } from 'grammy';
 import { BotModule } from './bot/bot.module';
 
 @Module({
@@ -11,6 +12,7 @@ import { BotModule } from './bot/bot.module';
       useFactory: (configService: ConfigService) => ({
         token: configService.get<string>('TELEGRAM_BOT_TOKEN'),
         botName: configService.get<string>('TELEGRAM_BOT_NAME'),
+        middlewares: [session()],
       }),
       inject: [ConfigService],
     }),
